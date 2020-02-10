c7d4b0fd54ecd25b934e5fc941e57f2502c6ac1b
twayland <twayland@fwparker.org>
2020-02-09 20:42:45 -0600

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20

--- Labs/hangman.py
+++ Labs/hangman.py
@@ -17,9 +17,6 @@
 
 # ask if they want to play again
 
-# tips
-# use .UPPER or.lower
-
 
 gallows = [
     '''
@@ -88,27 +85,46 @@ gallows = [
     ]
 
 import random
-word_list = ["Iowa", "Sanders", "Caucus", "Biden", "Buttigieg", "Electability"]
-word = word_list[random.randrange(0, (len(word_list) + 1))]
-abcs = [chr(x) for x in range(65, 65+26)] # 65 is capital A on ASCII chart
-incorrect_guesses = 0
-done = False
+word_list = ["IOWA", "SANDERS", "CAUCUS", "BIDEN", "WARREN", "PRIMARY"]  # options for hangman
+abcs = [chr(x) for x in range(65, 65+26)]  # 65 is capital A on ASCII chart  # alphabet
+incorrect_guesses = []
+correct_guesses = []
+my_word = word_list.pop(random.randrange(len(word_list)))  # pick random word
+blanks = len(my_word)  # to keep track of win
+done = False  # for loop
 
-print(gallows[0])
+print("Welcome to Iowa Caucus Hangman!")
+
 while not done:
-    for letter in word:
-        print("_")
-    guess = input("Guess a letter: ").upper()
-    if guess in word:
-        print("Correct Guess!")
+    print("Wrong guesses: ", incorrect_guesses)  # so user knows wrong guesses
+    print(gallows[len(incorrect_guesses)])  # print gallows depending on incorrect answers
+    correct = True
+    for letter in my_word:  # print blanks or letter depending on guess
+        if letter in correct_guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print()
+    user_guess = input("Guess a letter: ").upper()  # user input
 
-    if guess not in word:
-        incorrect_guesses += 1
-        print(gallows[incorrect_guesses])
-    if incorrect_guesses > 8:
+    if user_guess in abcs:  # check in alphabet
+        if user_guess in correct_guesses or user_guess in incorrect_guesses:  # check that they haven't guessed
+            print("You already guessed this.")
+        if user_guess not in my_word: # check if it's in word
+            print("Incorrect guess.")
+            incorrect_guesses.append(user_guess)
+        elif user_guess in my_word:  # if is in word, make used, give feedback
+            correct_guesses.append(user_guess)
+            print("Correct Guess!")
+            blanks -= my_word.count(user_guess)  # to track win
+    else:
+        print("Not a letter. Try again!")
+
+    if blanks <= 0:
+        print("The word was " + my_word + ".")
+        print("You guessed the word. Congrats, winner.")
+        done = True
+    if len(incorrect_guesses) == 6:
+        print("The word was" + my_word + ".")
         print("You lost!")
-        # replay = input(str(("Do you want to play again? Y or N: "))
-        # if replay == Y.upper:
-            # done = False
-        # if replay == N.upper:
-            # done = True
+        done = True
