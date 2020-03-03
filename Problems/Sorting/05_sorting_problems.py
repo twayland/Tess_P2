a766c00ba08dff50e67408375a5f8b1d1cbdfcb2
twayland <twayland@fwparker.org>
2020-03-02 22:32:23 -0600

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/04_searching.py
    #	Notes/resources/super_villains.txt

--- Problems/Sorting/05_sorting_problems.py
+++ Problems/Sorting/05_sorting_problems.py
@@ -7,9 +7,30 @@ I pulled this data from the csv in the same folder and converted it into a list 
 For all answers, show your work
 Use combinations of sorting, list comprehensions, filtering or other techniques to get the answers.
 '''
-from NBAStats import *
+import csv
+
+with open("/Users/tesswayland/PycharmProjects/P2_SP20/Problems/Sorting/Seasons_Stats.csv") as f:
+    read = csv.reader(f)
+    data = list(read)
+
+headers = data.pop(0)
+
+new_data = []
+
+for player in data:
+    new = []
+    for i in range(len(player)):
+        try:
+            new.append(float(player[i]))
+        except:
+            new.append(0)
+
+    new_data.append(new)
+
+print(new_data[0])
+print(new_data)
 #1  Pop off the first item in the list and print it.  It contains the column headers. (1pt)
-header = data.pop(0)
+header = new_data.pop(0)
 print(header)
 # print(header[2])
 '''
@@ -23,43 +44,43 @@ print(header)
 #2  Print the names of the top ten highest scoring single seasons in NBA history?
 # You should use the PTS (points) column to sort the data. (4pts)
 
-data.sort(key=lambda a: a[-1])
+new_data.sort(reverse=True, key=lambda a: a[-1])
 
-new_list = data[-10:]
+new_list = new_data[10:]
 for player in new_list:
     print(player[2])
 
 #3  How many career points did Kobe Bryant have? Add up all of his seasons. (4pts)
 career_points = 0
-for player in data:
+for player in new_data:
     if player[2] == "Kobe Bryant":
         career_points += player[-1]
 
 print("Kobe Bryant had", career_points, "career points.")
 
 #4  What player has the most 3point field goals in a single season. (3pts)
-data.sort(key=lambda a: a[-19])
-print(data[-1][2])
+new_data.sort(key=lambda a: a[-19])
+print(new_data[-1][2])
 
 #5  One stat featured in this data set is Win Shares(WS).
 #  WS attempts to divvy up credit for team success to the individuals on the team.
 #  WS/48 is also in this data.  It measures win shares per 48 minutes (WS per game).
 #  Who has the highest WS/48 season of all time?
 
-data.sort(key=lambda a: a[-28])
-print(data[-1][2], "had the highest WS/48 season of all time.")
+new_data.sort(key=lambda a: a[-28])
+print(new_data[-1][2], "had the highest WS/48 season of all time.")
 
 #6  Write your own question that you have about the data and provide an answer (4pts)
 # Maybe something like: "Who is the oldest player of all time?"  or "Who played the most games?"  or "Who has the most combined blocks and steals?".
 
-# Who is the youngest player of all time?
-data.sort(key=lambda a: a[5])
-print(data[0][2], "was the youngest player of all time.")
+# How old was the youngest player of all time?
+new_data.sort(key=lambda a: a[4])
+print(new_data[0][2], "was", new_data[0][4], "years old.")
 
 #7  Big challenge, few points.  Of the 100 highest scoring single seasons in NBA history, which player has the
 # worst free throw percentage?  Which had the best? (2pts)
-data.sort(key=lambda a: a[-1])
-challenge_list = data[-100:]
+new_data.sort(key=lambda a: a[-1])
+challenge_list = new_data[-100:]
 challenge_list.sort(key=lambda a: a[-10])
 print("Worst free throw percentage is", challenge_list[0][2])
 print("Best free throw percentage is", challenge_list[-1][2])
