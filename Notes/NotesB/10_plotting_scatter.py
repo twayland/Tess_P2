3a38c888cdb3255f600bfc703e8e0545b9c991e5
twayland <twayland@fwparker.org>
2020-03-25 13:48:03 -0500

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/10_plotting_csv.py

--- Notes/NotesB/10_plotting_scatter.py
+++ Notes/NotesB/10_plotting_scatter.py
@@ -5,9 +5,7 @@ import numpy as np
 with open("World firearms murders and ownership - Sheet 1.tsv") as f:
     reader = csv.reader(f, delimiter='\t')
     data = list(reader)
-
 print(data)
-
 header = data.pop(0)
 print(header)
 
@@ -15,11 +13,12 @@ print(header)
 homicide_100k = []
 firearms_100 = []
 countries = []
-
+similar = ["United States", "Canada", "England and Wales", "Japan", "South Korea", "France", "Germany", "Iceland", "Spain", "Belgium", "Netherlands", "Finland", "Singapore", "Israel", "Taiwan", "China", "Ireland"]
+data = [x for x in data if x[0] in similar]
 for country in data:
     try:
         homicides = float(country[5])
-        firearms = float(country([-2]))
+        firearms = float(country[-2])
         name = country[0]
         homicide_100k.append(homicides)
         firearms_100.append(firearms)
@@ -29,3 +28,17 @@ for country in data:
 
 print(countries)
 
+plt.figure("Homicides per Firearm Worldwide", figsize=(10, 6))  # add figsize
+plt.scatter(firearms_100, homicide_100k)
+plt.ylabel("homicides per 100k population")
+plt.xlabel("firearms per 100 people")
+plt.title("Homicides vs. Gun Ownership by Country")
+
+for i in range(len(countries)):
+    plt.annotate(countries[i], xy=(firearms_100[i], homicide_100k[i]))
+
+
+
+
+
+plt.show()
