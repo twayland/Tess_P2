ccb71434eb5cd0a038cfa8115a4673de1fc31d1d
twayland <twayland@fwparker.org>
2020-04-10 16:57:18 -0500

    Merge branch 'master' of https://www.github.com/fwparkercode/P2_SP20
    # Conflicts:
    #	Notes/NotesB/10_plotting_scatter.py

--- Labs/School Greenhouse Gas Emissions/ghg_lab.py
+++ Labs/School Greenhouse Gas Emissions/ghg_lab.py
@@ -34,8 +34,94 @@ Maybe you can try one of the following or think up your own:
 - Make schools in bottom 10 percent GHG Intesity show in red.
 - Add colleges and universities (use a different marker type)
 
-
 Note 2:  This is a tough assignment to do on your own.  Do your best with what you have.
 '''
 
+import csv
+import requests
+import matplotlib.pyplot as plt
+import numpy as np
+
+url = "https://data.cityofchicago.org/api/views/xq83-jr8c/rows.csv?accessType=DOWNLOAD"
+
+
+def get_data(url):
+    with requests.Session() as s:
+        download = s.get(url)
+        content = download.content.decode('utf-8')
+        reader = csv.reader(content.splitlines(), delimiter=',')
+        my_list = list(reader)
+    return my_list
+
+
+data = get_data("https://data.cityofchicago.org/api/views/xq83-jr8c/rows.csv?accessType=DOWNLOAD")
+header = data.pop(0)
+print(header)
+
+ghg_index = header.index("Total GHG Emissions (Metric Tons CO2e)")
+sqft_index = header.index("Gross Floor Area - Buildings (sq ft)")
+type_index = header.index("Primary Property Type")
+valid_data = []
+names = []
+
+# find valid data
+for building in data:
+    try:
+        int(building[ghg_index])
+        int(building[sqft_index])
+        name = building[2]
+        if building[0] == "2018" and building[type_index] == "K-12 School":
+            valid_data.append(building)
+            names.append(name)
+    except:
+        pass
+
+# make list of greenhouse gases and square footage
+ghg = [int(x[ghg_index]) for x in valid_data]
+ghg.sort()
+
+sqft = [int(x[sqft_index]) for x in valid_data]
+sqft.sort()
+
+# plot
+plt.figure(1, tight_layout=True)
+
+# best fit
+p = np.polyfit(sqft, ghg, 1)  # (x, y, poly_order)  1st order is linear
+print(p)  # returns (m, b)
+
+x = [x for x in range(450000)]
+y = [p[0] * y + p[1] for y in x]
+
+plt.plot(x, y, c="black")
+plt.xlabel("Gross Floor Area (sq ft)")
+plt.ylabel("Total GHG Emissions (Metric Tons CO2e)")
+plt.title("Emissions vs. Square Footage in Chicago Schools (2018)")
+
+# francis w parker doesn't have valid 2018 data, so i used latin instead for my annotation
+for name in names:
+    if name == "Latin School of Chicago Upper School":
+        name_index = names.index(name)
+        plt.annotate("Latin", xy=(sqft[name_index], ghg[name_index]))
+
+# color for top ten percent intensity, for bottom ten percent
+color = []
+ten_percent = int(len(ghg) * 0.1)
+
+ghg_bottom10 = [x for x in ghg[:ten_percent]]
+ghg_top10 = [x for x in ghg[-ten_percent:]]
+print(ghg_top10)
+print(ghg_bottom10)
+
+for building in ghg:
+    if building in ghg_top10:
+        color.append("red")
+    elif building in ghg_bottom10:
+        color.append("green")
+    else:
+        color.append("blue")
+
+plt.scatter(sqft, ghg, alpha=0.6, c=color)
+
+plt.show()
 
